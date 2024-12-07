c6e0c49b6e031a9aeeb80d6de565a0c416853574
natekimball <natekimball1@gmail.com>
2024-12-07 17:57:12 -0500

    printed input for random walk on expanders

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import time
 
 import numpy as np
-from core.weak_owf import weak_owf, random_permutation
-from core.hardness_amp import create_expander_graph, direct_product_construction, expander_construction, generate_random_inputs
+from src.weak_owf import weak_owf, random_permutation
+from src.hardness_amp import create_expander_graph, direct_product_construction, expander_construction, generate_random_inputs
 
 n = 10
 q = 10
@@ -11,15 +11,16 @@ inputs = generate_random_inputs(n, q)
 result = direct_product_construction(weak_owf, inputs)
 
 print("inputs:", inputs)
-print("Direct Product Construction output (hex):", result)
+print("Direct Product Construction output (hex):", result, end='\n\n')
 
-n = 2**16
+n = 2**4
 d = 10
 expander = create_expander_graph(n, d)
 pi = random_permutation(n)
 steps = np.random.randint(0, d, size=10)
-
 input = 0
+
+print(f"input: {input}{''.join(format(step,'x') for step in steps)}")
 result = expander_construction(expander, start=input, steps=steps, pi=pi)
 
 print("Random Walk on Expanders Construction output (hex):", result)
